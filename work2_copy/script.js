0f5483a1de082e28e2917c8d91334320ab983a39
anzu <56545404+apctech1@users.noreply.github.com>
2020-06-14 14:17:41 +0900

    Add files via upload

--- work2_copy/script.js
+++ work2_copy/script.js
@@ -1,10 +1,7 @@
 {
 	'use strict';
 
-	let latG = 0;
-	let lonG = 0;
-
-	navigator.geolocation.getCurrentPosition(success, fail);
+	// navigator.geolocation.getCurrentPosition(success, fail);
 
 	function success(pos) {
 		load.classList.add('dnone');
@@ -21,13 +18,61 @@
 	function utcToJSTime(utcTime) {
 		return utcTime * 1000;
 	}
+
+	let latG, lonG = '';
 	const load = document.getElementById('loading');
 
 	// 天気予報
 	function ajaxRequest(lat, lon) {
-		load.classList.add('dnone');
-		$('h2').html(lat + ',' + lon);
+		const url = `https://api.openweathermap.org/data/2.5/forecast?appid=dd64b46ea595c4104e0881953cb4e287&lat=${lat}&lon=${lon}&cnt=10&units=metric&lang=ja`;
 
+		fetch(url, {
+			mode: 'cors',
+			method: 'POST',
+			cache: 'no-cache',
+		})
+		.then(async response => {
+			const data = await response.json();
+			console.log(data);
+
+			// 都市
+			$('h2').html(data.city.name);
+
+			// 天気予報データ
+			data.list.forEach(function (forecast, index) {
+				const dateTime = new Date(utcToJSTime(forecast.dt));
+				const month = dateTime.getMonth() + 1;
+				const date = dateTime.getDate();
+				const hours = dateTime.getHours();
+				const min = String(dateTime.getMinutes()).padStart(2, '0');
+				const temperature = Math.round(forecast.main.temp);
+				const description = forecast.weather[0].description;
+				const iconPath = `../work2/img/${forecast.weather[0].icon}.svg`;
+				// 現在の天気、それ以外
+				if (index === 0) {
+					const currentWeather = `
+							<div class="info">
+								<p>
+									現在の天気：${description}
+									<span class="temp">${temperature}</span>°C
+								</p>
+							</div>
+							<div class="icon"><img src="${iconPath}"></div>`;
+					$('#weather').html(currentWeather);
+				}
+				else {
+					const tableRow = `
+							<tr>
+								<td class="info">
+									${month}/${date}<br>${hours}:${min}
+								</td>
+								<td class="icon"><img src="${iconPath}"></td>
+								<td><span class="description">${description}</span></td>
+								<td><span class="temp">${temperature}°C</span></td>
+							</tr>`;
+					$('#forecast').append(tableRow);
+				}
+			});
 
 			//2回目以降用
 			setTimeout(function() {
@@ -35,6 +80,10 @@
 					load.classList.add('dnone');
 				}
 			}, 800);
+		})
+		.catch(erro => {
+			console.log(erro);
+		});
 	}
 
 
@@ -46,7 +95,7 @@
 			load.classList.add('dnone');
 			ajaxRequest(35.68036537, 139.77166874);
 		}
-	}, 5000)
+	}, 1000)
 
 
 	// overflow
@@ -122,17 +171,18 @@
 
 	// 地名検索
 	areaList.forEach(function(list) {
-		$('#select_area').append(`<li>${list.area}</li>`);
+		$('#select_area').append(`<li data-coord="${list.coord}">${list.area}</li>`);
 	});
 
 	const li = document.querySelectorAll('#select_area li');
 	for (let i = 0; i < li.length; i++) {
 		li[i].addEventListener('click', function() {
-			latG = areaList[i]['coord'][0];
-			lonG = areaList[i]['coord'][1];
-			ajaxRequest(latG, lonG);
+			let coord = this.dataset.coord.split(',');
+			console.log(coord[0]);
+			ajaxRequest(coord[0], coord[1]);
 
 			overflow.classList.add('hidden');
+			load.classList.remove('dnone');
 			$('#forecast').html('');
 			$('#weather').html('');
 		});
